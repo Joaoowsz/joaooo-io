e1140dddd59032f4956a3362e023cfb76fb1d2cf
João Catozzi <jao_catozzi@hotmail.com>
2018-05-30 10:27:58 -0300

    .

--- index.js
+++ index.js
@@ -84,18 +84,36 @@ bot.on("message", function(user, userID, channelID, message, event) {
                 })
     });
 
-    bot.on("guildMemberAdd", function(member, user, userID, channelID, message, event) {
-      if(message.startsWith("/anunciar")){
-        bot.sendMessage("@everyone")
-        to: "446481883346501652"
-        serverID: "446476615053213697"
-        embed: {
-          fields: [
-            {
-              name: "Anúncio",
-              value: message.slice(10)
-            },
-          ]
+bot.on("message", function(user, userID, channelID, message, event) {
+    if(message.startsWith("/anunciar")) {
+      bot.sendMessage({
+        to:"446481883346501652",
+        message: "@everyone",
+      })
+        var avatar = "http://cdn.discordapp.com/avatars/" + userID + "/" + bot.users[userID].avatar;
+
+        bot.sendMessage ({
+            to:"446481883346501652",
+            "embed": {
+              "color":16570605,
+              "url": "https://discordapp.com",
+              "timestamp": new Date().toISOString(),
+              "footer": {
+                "icon_url": avatar,
+                "text": "Enviado por: " + user
+              },
+              "fields": [
+                {
+                  "name": "📢 Anúncio",
+                  "value": message.slice(10)
+                }
+              ]
+            }
+          })
+
+            bot.deleteMessage({
+                channelID: channelID,
+                messageID: event.d.id,
+              })
         }
-      }
     })
